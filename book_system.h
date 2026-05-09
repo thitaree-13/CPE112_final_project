a77615d4c53b51ad05e9fd8eea43fe51c43d2829
thitaree-13 <74484144+thitaree-13@users.noreply.github.com>
2026-05-09 19:59:33 +0700

    Revert "Update borrow system"

--- book_system.h
+++ book_system.h
@@ -13,43 +13,13 @@ typedef struct Book {
 
 } Book;
 
-//lib
+// ===== GLOBAL HEAD =====
+extern Book* head;
 
-typedef struct {
-
-    Book books[MAX_BOOKS];
-
-    int book_count;
-
-    //new
-    int user_borrow_count[MAX_USERS];
-
-} Library;
-
-//function
-
-void library_init(Library *lib);
-
-int library_add_book(Library *lib,
-                     const char *title);
-
-bool borrow_book(Library *lib,
-                 int book_id,
-                 int user_id);
-
-bool return_book(Library *lib,
-                 int book_id,
-                 int user_id);
-
-bool join_waitlist(Library *lib,
-                   int book_id,
-                   int user_id);
-
-bool is_book_available(Library *lib,
-                       int book_id);
-
-int get_waitlist_count(Library *lib,
-                       int book_id);
+// ===== FUNCTIONS =====
+void addBook();
+void deleteBook();
+void displayBooks();
 
 void saveBooksToFile();
 void loadBooksFromFile();
